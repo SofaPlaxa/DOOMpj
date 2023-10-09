06772a54bc7b75bde33d132e77191a546c7d5b58
Студент BStudentD <student@obe.local>
2023-10-09 17:55:02 +0300

    crosshair and raycast

--- Assets/Scripts/PlayerShoot.cs
+++ Assets/Scripts/PlayerShoot.cs
@@ -4,19 +4,66 @@ using UnityEngine;
 
 public class PlayerShoot : MonoBehaviour
 {
+
+    [SerializeField] UIAim aim;
+
     void Update()
     {
-        Debug.DrawLine(transform.position, transform.position + transform.forward * 100, Color.green);
+
+
         foreach(DamagableComponent enemy in EnemyManager.Enemies)
         {
-            Vector3 enemyDirection = (enemy.transform.position - transform.position).normalized;
-            //print(Mathf.Acos(Vector3.Dot))
+            Vector3 enemyDirection = enemy.transform.position - transform.position;
+
+            Vector3 enemyDirection2D = enemyDirection;
+            enemyDirection2D.y = 0;
+            enemyDirection2D = enemyDirection2D.normalized;
+
+
+            enemyDirection = enemyDirection.normalized;
+
+            float angle = Mathf.Acos(Vector3.Dot(transform.forward, enemyDirection2D)) * Mathf.Rad2Deg;
+
+            if(angle<3)
+            {
+
+                CapsuleCollider enemyCollider = enemy.GetComponent<CapsuleCollider>();
+
+                Vector3 unitFrac = new Vector3(0, enemyCollider.height / 2);
+
+
+                if (AimLineAttack(enemy.transform.position)
+
+                    || AimLineAttack(enemy.transform.position + unitFrac)
+                    || AimLineAttack(enemy.transform.position - unitFrac))
+                {
+                    aim.CanShoot = true;
+                    return;
+                }
+           
+            }
+
+            aim.CanShoot = false;
+
         }
 
-        //if(Physics.Raycast((transform.position, transform.forward, out RaycastHit hit) && hit.collider.TryGetComponent(out DamagableComponent damagable))
+        bool AimLineAttack(Vector3 targetPos)
+        {
+            if (Physics.Linecast(transform.position, targetPos, out RaycastHit hit)
+               && hit.collider.GetComponent<DamagableComponent>())
+            {
+
+                Debug.DrawLine(transform.position, targetPos, Color.green);
+               
+                return true;
+            }
+            return false;
+        }
+
+        //if (Physics.Raycast((transform.position, transform.forward, out RaycastHit hit) && hit.collider.TryGetComponent(out DamagableComponent damagable))
         //{
         //    Debug.Log("can shoot");
         //}
-        
+
     }
 }
---
+++ Assets/Scripts/UI/UIAim.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class UIAim : MonoBehaviour
+{
+
+    [SerializeField] Image aimImage;
+
+    public bool CanShoot { get; set; }
+
+    private void Update()
+    {
+        aimImage.color = CanShoot ? Color.red : Color.white;
+    }
+}
